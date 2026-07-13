62ec2c5d799fff098d1a5eb0056e857ab2dc82c2
Guido Magni <guibonmagni@gmail.com>
2026-07-13 14:12:59 +0200

    update

---
+++ 5/py_twist_sequence.py
@@ -0,0 +1,7 @@
+def twist_sequence(arr: list[int], k: int) -> list[int]:
+    if not arr:
+        return []
+    k %= len(arr)
+    return arr[-k:] + arr[:-k] if k else arr[:]
+
+print(twist_sequence([1,2,3,4,5], 2))
